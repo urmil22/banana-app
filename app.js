c9d57687079754bee2b876ec3c7a80f03df9687a
urmil22 <urmilbhavsar8@gmail.com>
2021-07-10 12:28:07 +0530

    added css and image

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output")
+var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-    outputDiv.innerText = "jireneiu "+ txtInput.value;
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
 }
 
-btnTranslate.addEventListener("click" , clickHandler)
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong with server! try again after sometime")
+}
+
+function clickHandler() {
+  
+    var inputText = txtInput.value; //input taking
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler)
